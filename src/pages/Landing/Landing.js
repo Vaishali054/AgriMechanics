67adbcb2884bfbf7882aa0c6e199964062896fdb
Vaishali054 <100770875+Vaishali054@users.noreply.github.com>
2023-12-30 16:21:09 +0530

    adding landing page

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from "../../components/navbar/NavBar"
+import Hero from '../../components/hero/Hero'
 
 export default function Landing() {
   return (
     <>
       <Navbar/>
+      <Hero/>
     </>
   )
 }
